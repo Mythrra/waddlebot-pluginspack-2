55e0a123eeb7cafc31b5f9039f62983f82a41d12
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-25 18:33:44 -0500

    waddle waddle waddle

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -15,16 +15,14 @@ class HelloPlugin(commands.Cog):
             await message.channel.send("Night and sweet dreams! :first_quarter_moon_with_face:")
         elif "and then he waddled away" in message.content.lower():
             await message.channel.send("*waddle waddle waddle*")
+        elif "until the very next day" in message.content.lower():
+            await message.channel.send("*Bum bum bum bum ba-bada-dum*")
         elif "do you think waddle dee should be in smash?" in message.content.lower():
             await message.channel.send("YES")
         elif "can we get an f in the chat" in message.content.lower():
             await message.channel.send(":regional_indicator_f:")
         elif "colors weave into a spire of flame" in message.content.lower():
             await message.channel.send("Distant sparks call to a past still unamed...")
-        elif "who's the best kirby in Waddling Gaming?" in message.content.lower():
-            await message.channel.send("probably prix")
-        elif "What is the best Kirby game?" in message.content.lower():
-            await message.channel.send("Super Star Ultra or the 3DS ones")
         elif "can we get a petfroge in the chat" in message.content.lower():
             await message.channel.send("https://images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
 
