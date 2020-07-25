0cb75b9ff66141483d6cb11e79ee9a3a9ec74cac
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-25 13:23:27 -0500

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -15,10 +15,10 @@ class HelloPlugin(commands.Cog):
             await message.channel.send("Night and sweet dreams! :first_quarter_moon_with_face:")
         elif "can we get a petfroge in the chat" in message.content.lower():
             await message.channel.send("https://images-ext-1.discordapp.net/external/eeq2rFAMpqdHOLusMvyLMo9U98gYi5mv9vhiqzgLbTY/https/images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
+        elif "do you think waddle dee should be in smash?" in message.content.lower():
+            await message.channel.send("<YES")
         elif "can we get an f in the chat" in message.content.lower():
-            await message.channel.send("<:KirbyF:705969779676151818>")
-        elif "can we get a pog in the chat" in message.content.lower():
-            await message.channel.send("<:KirbyPog:714882454275752026>")
+            await message.channel.send(":regional_indicator_f:")
 
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
