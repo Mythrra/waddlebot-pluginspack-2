b249a8fc8c1f60f54d04a412e3c9cba41a5948a5
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-25 14:38:39 -0500

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -13,14 +13,16 @@ class HelloPlugin(commands.Cog):
             await message.channel.send("Good Morning! :sunrise:")
         elif "good night" in message.content.lower():
             await message.channel.send("Night and sweet dreams! :first_quarter_moon_with_face:")
-        elif "Then he waddled away" in message.content.lower():
+        elif "and then he waddled away" in message.content.lower():
             await message.channel.send("*waddle waddle waddle*")
         elif "do you think waddle dee should be in smash?" in message.content.lower():
             await message.channel.send("YES")
         elif "can we get an f in the chat" in message.content.lower():
             await message.channel.send(":regional_indicator_f:")
-        elif "Colors weave into a spire of flame" in message.content.lower():
+        elif "colors weave into a spire of flame" in message.content.lower():
             await message.channel.send("Distant sparks call to a past still unamed...")
+        elif "can we get a petfroge in the chat" in message.content.lower():
+            await message.channel.send("https://images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
 
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
