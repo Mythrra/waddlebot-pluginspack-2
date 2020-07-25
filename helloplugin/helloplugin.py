9da4611832072f6abe7265257fb6bffe7014f14c
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-25 12:46:13 -0500

    added petfroge, edited good morning

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -18,11 +18,11 @@ class HelloPlugin(commands.Cog):
         elif "gn" in message.content.lower():
             await message.channel.send("Good Night")
         elif "good morning" in message.content.lower():
-            await message.channel.send("Good Morning !")
+            await message.channel.send("Good Morning :sunrise:!")
         elif "good night" in message.content.lower():
-            await message.channel.send("Good Night !")
-        elif "hey" in message.content.lower():
-            await message.channel.send("Hello !")
+            await message.channel.send("Night and sweet dreams :first_quarter_moon_with_face: !")
+        elif "petfroge" in message.content.lower():
+            await message.channel.send("https://images-ext-1.discordapp.net/external/eeq2rFAMpqdHOLusMvyLMo9U98gYi5mv9vhiqzgLbTY/https/images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
         elif "sup" in message.content.lower():
             await message.channel.send("Sup")
         elif "hi" in message.content.lower():
