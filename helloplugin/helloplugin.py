bd89b6139a5e9239aceb436d4f1d04dc15df3b2c
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-25 13:33:15 -0500

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -13,8 +13,8 @@ class HelloPlugin(commands.Cog):
             await message.channel.send("Good Morning! :sunrise:")
         elif "good night" in message.content.lower():
             await message.channel.send("Night and sweet dreams! :first_quarter_moon_with_face:")
-        elif "can we get a petfroge in the chat" in message.content.lower():
-            await message.channel.send("https://images-ext-1.discordapp.net/external/eeq2rFAMpqdHOLusMvyLMo9U98gYi5mv9vhiqzgLbTY/https/images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
+        elif "Then he waddled away" in message.content.lower():
+            await message.channel.send("*waddle waddle waddle*")
         elif "do you think waddle dee should be in smash?" in message.content.lower():
             await message.channel.send("YES")
         elif "can we get an f in the chat" in message.content.lower():
