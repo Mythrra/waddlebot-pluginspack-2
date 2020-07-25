cd568684d617a7858aa386cd4d4cf486f1541c6f
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-25 14:50:13 -0500

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -21,6 +21,8 @@ class HelloPlugin(commands.Cog):
             await message.channel.send(":regional_indicator_f:")
         elif "colors weave into a spire of flame" in message.content.lower():
             await message.channel.send("Distant sparks call to a past still unamed...")
+        elif "who's the best kirby in Waddling Gaming?" in message.content.lower():
+            await message.channel.send("probably prix")
         elif "can we get a petfroge in the chat" in message.content.lower():
             await message.channel.send("https://images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
 
