1662769219b91350c869ff0877bdf1428d0ea5e2
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-25 13:18:00 -0500

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -10,15 +10,15 @@ class HelloPlugin(commands.Cog):
             return
 
         elif "good morning" in message.content.lower():
-            await message.channel.send("Good Morning :sunrise:!")
+            await message.channel.send("Good Morning! :sunrise:")
         elif "good night" in message.content.lower():
-            await message.channel.send("Night and sweet dreams :first_quarter_moon_with_face: !")
-        elif "petfroge" in message.content.lower():
+            await message.channel.send("Night and sweet dreams! :first_quarter_moon_with_face:")
+        elif "can we get a petfroge in the chat" in message.content.lower():
             await message.channel.send("https://images-ext-1.discordapp.net/external/eeq2rFAMpqdHOLusMvyLMo9U98gYi5mv9vhiqzgLbTY/https/images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
         elif "can we get an f in the chat" in message.content.lower():
-            await message.channel.send(":KirbyF:")
+            await message.channel.send("F :KirbyF:")
         elif "can we get a pog in the chat" in message.content.lower():
-            await message.channel.send(":KirbyPog:!")
+            await message.channel.send("POG :KirbyPog:")
 
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
