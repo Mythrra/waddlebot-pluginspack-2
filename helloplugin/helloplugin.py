fd2e33f7ed141a339958a53af5509f2bf4acce16
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-26 16:26:26 -0500

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -9,14 +9,18 @@ class HelloPlugin(commands.Cog):
         if message.author.bot:
             return
 
-        elif "good morning" in message.content.lower():
+        elif "good morning guys" in message.content.lower():
             await message.channel.send("Good Morning! :sunrise:")
-        elif "good night" in message.content.lower():
+        elif "good afternoon guys" in message.content.lower():
+            await message.channel.send("Good Afternoon! :white_sun_cloud:")
+        elif "good night guys" in message.content.lower():
             await message.channel.send("Night and sweet dreams! :first_quarter_moon_with_face:")
         elif "and then he waddled away" in message.content.lower():
             await message.channel.send("*waddle waddle (waddle)*")
         elif "until the very next day" in message.content.lower():
-            await message.channel.send("*Bum bum bum bum ba-bada-dum*")
+            await message.channel.send("*Bum bum bum bum ba-da-dum*")
+         elif "hey, got any grapes?" in message.content.lower():
+            await message.channel.send("***N O***")
         elif "do you think waddle dee should be in smash?" in message.content.lower():
             await message.channel.send("YES")
         elif "can we get an f in the chat" in message.content.lower():
