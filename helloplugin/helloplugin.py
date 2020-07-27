3c8ae2bb50b6057db38d694d09dec65568b4e57e
superhylia <43157926+superhylia@users.noreply.github.com>
2020-07-27 16:31:06 -0500

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -9,7 +9,7 @@ class HelloPlugin(commands.Cog):
         if message.author.bot:
             return
 
-        elif "hey @WaddleBot" in message.content.lower():
+        elif "hey <@736351307841142915>" in message.content.lower():
             await message.channel.send("Hello! <:BandanaDeeWave:735641686348398713>")
         elif "good morning guys" in message.content.lower():
             await message.channel.send("Good Morning! :sunrise:")
@@ -29,6 +29,10 @@ class HelloPlugin(commands.Cog):
             await message.channel.send("Distant sparks call to a past still unamed...")
         elif "can we get a petfroge in the chat" in message.content.lower():
             await message.channel.send("https://images-ext-2.discordapp.net/external/H3akJEvJbTEc1ZPGqEU4wapDdZDQhOyul_9Bh3KkStU/https/cdn.discordapp.com/emojis/731940867547594793.gif")
+        elif "can I get a high five? " in message.content.lower():
+            await message.channel.send(f"{ctx.author.mention} :orange_heart: https://media.discordapp.net/attachments/698534935723507755/737420105721577622/KirbyHigh52.gif")
+        elif "can I get a hug? " in message.content.lower():
+            await message.channel.send(f"{ctx.author.mention} :orange_heart: https://media.discordapp.net/attachments/698534935723507755/737420867591471194/KirbyHug.gif")
 
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
